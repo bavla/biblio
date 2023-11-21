381d8fdc51cd7c722035706f8f5fbdf86797d12a
bavla <vladimir.batagelj@gmail.com>
2023-11-21 04:02:15 +0100

    Add files via upload

---
+++ code/bibmat.R
@@ -0,0 +1,12 @@
+# bibmat - Derived bibliographic networks
+# Vladimir Batagelj, November 2023
+
+normalize <- function(M) t(apply(M,1,function(x) x/max(1,sum(x))))
+newman <- function(M) t(apply(M,1,function(x) x/max(1,sum(x)-1)))
+D0 <- function(M) {diag(M) <- 0; return(M)}
+binary <- function(M) {B <- t(apply(M,1,function(x) as.integer(x!=0)))
+  colnames(B) <- colnames(M); return(B)}
+wodeg <- function(M) apply(M,1,sum)
+wideg <- function(M) apply(M,2,sum)
+odeg <- function(M) wodeg(binary(M))
+ideg <- function(M) wideg(binary(M))
